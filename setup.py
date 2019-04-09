51d77625064827e7b3693df9d70894cb1255e645
José Carlos López Arriaga <44308974+CarlosLopezNubes5@users.noreply.github.com>
2019-04-09 18:13:37 -0500

    Update

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='RungeKutta',
-    version='',
-    packages=[''],
-    url='',
+    version='1.0',
+    packages=['RK4, RK4diffsys'],
+    url='https://github.com/CarlosLopezNubes5/RungeKutta',
     license='',
     author='Jose Carlos Lopez Arriaga; Ana Isabel Gutierrez Chavez',
     author_email='',
-    description='', install_requires=['numpy', 'matplotlib']
+    description='Implementation of the Runge Kutta 4th order method for ODEs',
+    install_requires=['numpy', 'matplotlib']
 )
