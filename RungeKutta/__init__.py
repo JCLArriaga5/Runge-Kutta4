0ca167a10b20eb96a932380670cc76f962a117ea
José Carlos López Arriaga <44308974+CarlosLopezNubes5@users.noreply.github.com>
2020-12-18 18:21:47 -0600

    Update Code

--- RungeKutta/__init__.py
+++ RungeKutta/__init__.py
@@ -4,13 +4,3 @@
 #
 # Implementation of the Runge Kutta method of fourth order, a small module was implemented to solve ordinary
 # differential equations of the first order, the purposes are educational and learning by the authors.
-
-from __future__ import division
-import sys
-import os
-abspath = os.getcwd()
-dirpath = abspath.replace('/RungeKutta/GUI-RK4', '/')
-sys.path.append(dirpath)
-from RungeKutta.RK4 import *
-
-np.set_printoptions(suppress=True)
