c9bcaf32dc1d752c0a36415df153b1a26db58eaa
José Carlos López Arriaga <44308974+CarlosLopezNubes5@users.noreply.github.com>
2020-12-18 19:12:08 -0600

    Update Code

--- RungeKutta/GUI-RK4/GUI.py
+++ RungeKutta/GUI-RK4/GUI.py
@@ -27,23 +27,23 @@ import sympy
 (t, y, e) = sympy.symbols("t, y, e")
 
 
-class RungeKuttaGUI:
+class RK4GUI:
     def __init__(self, master):
         # Window design
         self.master = master
         gui_bg = "#4F5251"
 
         # Layout
-        img_tmp = Image.open(abspath + '/images/layout-rk4.png')
-        img_tmp = img_tmp.resize((1202, 602), Image.ANTIALIAS)
-        layout_img = ImageTk.PhotoImage(img_tmp)
-        lyout = Label(master, image=layout_img, bg=gui_bg)
-        lyout.layout_img=layout_img
-        lyout.place(x=-2, y=-2)
-        # Working ...
+        # img_tmp = Image.open(abspath + '/images/layout-rk4.png')
+        # img_tmp = img_tmp.resize((1202, 602), Image.ANTIALIAS)
+        # layout_img = ImageTk.PhotoImage(img_tmp)
+        # lyout = Label(master, image=layout_img, bg=gui_bg)
+        # lyout.layout_img=layout_img
+        # lyout.place(x=-2, y=-2)
+        # --> Working ...
 
         master.title("Runge Kutta 4th Order")
-        # master.config(bg=gui_bg)
+        master.config(bg=gui_bg)
         master.geometry("1200x600")
         master.resizable(width=FALSE, height=FALSE)
 
@@ -54,10 +54,8 @@ class RungeKuttaGUI:
             icon = PhotoImage(file=abspath + '/images/RK4-logo.png')
             master.tk.call('wm', 'iconphoto', master._w, icon)
         if self.OS == 'win32':
-            # icon = PhotoImage(file='images/RK4-logo.png')
-            # master.tk.call('wm', 'iconphoto', master._w, icon)
-
-            master.wm_iconbitmap(default=abspath + '/images/RK4-logo.ico') # For Windows system show icon
+            # For Windows system show icon
+            master.wm_iconbitmap(default=abspath + '/images/RK4-logo.ico')
             myappid = 'Isa-Carlos.RungeKutta.RK4.1-1'  # arbitrary string
             ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
@@ -68,11 +66,6 @@ class RungeKuttaGUI:
         self.canvas = FigureCanvasTkAgg(self.fig, self.master)
         self.canvas.draw()
 
-        # Initialize Runge-Kutta firstorder ode
-        self.methd = firstorder(self.f)
-        self.ts = 0
-        self.ys = 0
-
         titletext = Label(master, text="Runge Kutta 4th Order",
                           bg=gui_bg, fg="white", font="time 20 bold")
         titletext.pack(side='top')
@@ -80,55 +73,67 @@ class RungeKuttaGUI:
         titleODE = Label(master, text="Ordinary differential equations of the first order",
                          bg=gui_bg, fg="white", font="time 12 bold").place(x=90, y=40)
 
-        label_equation = Label(master, text='Enter equation: dy/dt =', bg='#689E8C').place(x=110, y=80, width=180, height=40)
+        label_equation = Label(master, text='Enter equation: dy/dt =',
+                               bg='#689E8C').place(x=110, y=80, width=180, height=40)
 
         self.equation = StringVar()
-        self.equation.set('2*t - 3*y + 1')
-        equantion_entry = Entry(master, width=12, textvariable=self.equation).place(x=290, y=80, width=150, height=40)
+        self.equation.set('2 * t - 3 * y + 1')
+        equantion_entry = Entry(master, width=12,
+                                textvariable=self.equation).place(x=290, y=80, width=150, height=40)
 
-        self.label_parameters = Label(master, text='Parameters', bg='#476B5F').place(x=225, y=130, width=100, height=40)
+        self.label_parameters = Label(master, text='Parameters',
+                                      bg='#476B5F').place(x=225, y=130, width=100, height=40)
 
         # ti parameter
-        self.label_ti = Label(master, text='ti:', bg='#689E8C').place(x=110, y=170, width=40, height=25)
+        self.label_ti = Label(master, text='ti :',
+                              bg='#689E8C').place(x=110, y=170, width=40, height=25)
         self.ti = DoubleVar()
         self.ti.set('1.0')
-        self.entry_ti = Entry(master, width=7, textvariable=self.ti).place(x=150, y=170, width=40, height=25)
+        self.entry_ti = Entry(master, width=7,
+                              textvariable=self.ti).place(x=150, y=170, width=40, height=25)
 
         # yi parameter
-        self.label_yi = Label(master, text='yi:', bg='#689E8C').place(x=190, y=170, width=40, height=25)
+        self.label_yi = Label(master, text='yi :',
+                              bg='#689E8C').place(x=190, y=170, width=40, height=25)
         self.yi = DoubleVar()
         self.yi.set('5.0')
-        self.entry_yi = Entry(master, width=7, textvariable=self.yi).place(x=220, y=170, width=40, height=25)
+        self.entry_yi = Entry(master, width=7,
+                              textvariable=self.yi).place(x=220, y=170, width=40, height=25)
 
-        # done parameter
-        self.label_done = Label(master, text='done:', bg='#689E8C').place(x=260, y=170, width=40, height=25)
-        self.done = DoubleVar()
-        self.done.set('1.5')
-        self.entry_done = Entry(master, width=7, textvariable=self.done).place(x=300, y=170, width=40, height=25)
+        # t parameter
+        self.label_t = Label(master, text='t :',
+                             bg='#689E8C').place(x=260, y=170, width=40, height=25)
+        self.t = DoubleVar()
+        self.t.set('1.5')
+        self.entry_t = Entry(master, width=7,
+                             textvariable=self.t).place(x=300, y=170, width=40, height=25)
 
         # h parameter
-        self.label_h = Label(master, text='h:', bg='#689E8C').place(x=340, y=170, width=40, height=25)
+        self.label_h = Label(master, text='h :', bg='#689E8C').place(x=340, y=170, width=40, height=25)
         self.h = DoubleVar()
-        self.h.set('0.1')
+        self.h.set('0.01')
         self.entry_h = Entry(master, width=7, textvariable=self.h).place(x=380, y=170, width=40, height=25)
 
         # computed
-        compute = Button(master, text='Compute', command=self.solve, relief='raised', bg='#989E9C').place(x=180, y=200, width=80, height=30)
+        compute = Button(master, text='Compute', command=self.solve, relief='raised',
+                         bg='#989E9C').place(x=180, y=200, width=80, height=30)
 
         self.computed = DoubleVar()
-        self.label_computed = Label(master, textvariable=self.computed, width=20).place(x=260, y=200, width=150, height=30)
+        self.label_computed = Label(master, textvariable=self.computed,
+                                    width=20).place(x=260, y=200, width=150, height=30)
 
         # Graph
-        graph_label = Label(master, text='Graph the solution with respect to variable t',
-                            bg="#4F5251", fg="white", font="time 12 bold").place(x=680, y=40)
-        graph_button = Button(master, text='Graph Function', command=self.graph, relief='raised', bg='#989E9C').place(x=770, y=80, width=200, height=20)
+        graph_label = Label(master, text='Solution graph',
+                            bg="#4F5251", fg="white", font="time 14 bold").place(x=790, y=40)
+        graph_button = Button(master, text='Graph', command=self.graph,
+                              relief='raised', bg='#989E9C').place(x=770, y=80, width=200, height=20)
 
-        self.button_close = Button(master, text='Close', bg='#E1EBE7', fg="black", command=self.exit).place(x=1100, y=550, width=80, height=30)
+        self.button_close = Button(master, text='Close', bg='#E1EBE7', fg="black",
+                                   command=self.exit).place(x=1100, y=550, width=80, height=30)
 
-    # noinspection PyUnusedLocal
     def f(self, t, y):
         """
-        Function to declare the function to solve in the RK4 library format.
+        Declare function to solve in the RK4 library format.
 
         Parameters
         ----------
@@ -149,30 +154,29 @@ class RungeKuttaGUI:
         """
 
         self.ax.clear()
-        computed = self.methd.solve(np.double(self.ti.get()), np.double(self.yi.get()), np.double(self.done.get()),
-                                  np.double(self.h.get()))
-        self.computed.set(computed)
+        # Initialize Runge-Kutta firstorder ode
+        methd = firstorder(self.f)
+        r = methd.solve(np.double(self.ti.get()), np.double(self.yi.get()),
+                        np.double(self.t.get()), np.double(self.h.get()))
+        # Obtain values of solution
+        self.ts, self.ys = methd.get_vals()
+        self.computed.set(r)
 
     def graph(self):
         """
-        To use the -graph- function of RK4 and display the graph solution
+        Graph values of each iteration of method
         """
-        # Show in GUI
-        # Obtain values of solution
-        self.ts, self.ys = self.methd.graphvalues()
+
         self.ax.clear()
         self.ax.set_title(r'Solution graph of  $\frac{dy}{dt}= %s$' % sympy.latex(eval(self.equation.get())))
         self.ax.plot(self.ts, self.ys, 'r')
-        self.ax.legend('y')
+        # self.ax.legend('Solution curve')
         self.ax.grid()
         self.ax.set_xlabel("$ t $")
         self.ax.set_ylabel("$ y(t) $")
 
         self.canvas.draw()
         self.canvas.get_tk_widget().place(x=601, y=123)
-
-        # Clear values for next solutions
-        self.methd.emptyvalues()
 
     def exit(self):
         """
@@ -184,7 +188,7 @@ class RungeKuttaGUI:
 
 def main():
     root = Tk()
-    rk = RungeKuttaGUI(root)
+    rk = RK4GUI(root)
     root.mainloop()
 
 if __name__ == '__main__':
